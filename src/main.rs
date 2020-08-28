385570df8c752a90bd98698018ee0c0340b19325
Alan Somers <asomers@axcient.com>
2020-08-28 19:44:19 +0000

    Report metrics in mostly the same format as node_exporter's nfsd collector
    It's not exactly the same format because:
    * we need our own namespace (nfs vs node)
    * FreeBSD doesn't collect separate stats for each protocol version

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use env_logger::{
 use prometheus_exporter::{
     FinishedUpdate,
     PrometheusExporter,
-    prometheus::register_int_gauge,
+    prometheus::register_int_gauge_vec,
 };
 use std::{
     convert::TryInto,
@@ -31,47 +31,9 @@ fn main() {
     // Create metrics
     // Even though these are gauge, we use the Gauge API since the kernel
     // reports their current values.
-    let getattr = register_int_gauge!("getattr", "Count of server GETATTR RPCs")
-        .expect("can not create gauge");
-    let setattr = register_int_gauge!("setattr", "Count of server SETATTR RPCs")
-        .expect("can not create gauge");
-    let lookup = register_int_gauge!("lookup", "Count of server LOOKUP RPCs")
-        .expect("can not create gauge");
-    let readlink = register_int_gauge!("readlink", "Count of server READLINK RPCs")
-        .expect("can not create gauge");
-    let read = register_int_gauge!("read", "Count of server READ RPCs")
-        .expect("can not create gauge");
-    let write = register_int_gauge!("write", "Count of server WRITE RPCs")
-        .expect("can not create gauge");
-    let create = register_int_gauge!("create", "Count of server CREATE RPCs")
-        .expect("can not create gauge");
-    let remove = register_int_gauge!("remove", "Count of server REMOVE RPCs")
-        .expect("can not create gauge");
-    let rename = register_int_gauge!("rename", "Count of server RENAME RPCs")
-        .expect("can not create gauge");
-    let link = register_int_gauge!("link", "Count of server LINK RPCs")
-        .expect("can not create gauge");
-    let symlink = register_int_gauge!("symlink", "Count of server SYMLINK RPCs")
-        .expect("can not create gauge");
-    let mkdir = register_int_gauge!("mkdir", "Count of server MKDIR RPCs")
-        .expect("can not create gauge");
-    let rmdir = register_int_gauge!("rmdir", "Count of server RMDIR RPCs")
-        .expect("can not create gauge");
-    let readdir = register_int_gauge!("readdir", "Count of server READDIR RPCs")
-        .expect("can not create gauge");
-    let readdirplus = register_int_gauge!("readdirplus", "Count of server READDIRPLUS RPCs")
-        .expect("can not create gauge");
-    let access = register_int_gauge!("access", "Count of server ACCESS RPCs")
-        .expect("can not create gauge");
-    let mknod = register_int_gauge!("mknod", "Count of server MKNOD RPCs")
-        .expect("can not create gauge");
-    let fsstat = register_int_gauge!("fsstat", "Count of server FSSTAT RPCs")
-        .expect("can not create gauge");
-    let fsinfo = register_int_gauge!("fsinfo", "Count of server FSINFO RPCs")
-        .expect("can not create gauge");
-    let pathconf = register_int_gauge!("pathconf", "Count of server PATHCONF RPCs")
-        .expect("can not create gauge");
-    let commit = register_int_gauge!("commit", "Count of server COMMIT RPCs")
+    let rpcs = register_int_gauge_vec!("nfs_nfsd_requests_total",
+                                       "Count of server RPCs",
+                                       &["method"])
         .expect("can not create gauge");
 
     loop {
@@ -80,28 +42,27 @@ fn main() {
 
         // Update metric with random value.
         let nfs_stat = nfs::collect().unwrap();
-        getattr.set(nfs_stat.getattr.try_into().unwrap());
-        setattr.set(nfs_stat.setattr.try_into().unwrap());
-        lookup.set(nfs_stat.lookup.try_into().unwrap());
-        readlink.set(nfs_stat.readlink.try_into().unwrap());
-        read.set(nfs_stat.read.try_into().unwrap());
-        write.set(nfs_stat.write.try_into().unwrap());
-        create.set(nfs_stat.create.try_into().unwrap());
-        remove.set(nfs_stat.remove.try_into().unwrap());
-        rename.set(nfs_stat.rename.try_into().unwrap());
-        link.set(nfs_stat.link.try_into().unwrap());
-        symlink.set(nfs_stat.symlink.try_into().unwrap());
-        mkdir.set(nfs_stat.mkdir.try_into().unwrap());
-        rmdir.set(nfs_stat.rmdir.try_into().unwrap());
-        readdir.set(nfs_stat.readdir.try_into().unwrap());
-        readdirplus.set(nfs_stat.readdirplus.try_into().unwrap());
-        access.set(nfs_stat.access.try_into().unwrap());
-        mknod.set(nfs_stat.mknod.try_into().unwrap());
-        fsstat.set(nfs_stat.fsstat.try_into().unwrap());
-        fsinfo.set(nfs_stat.fsinfo.try_into().unwrap());
-        pathconf.set(nfs_stat.pathconf.try_into().unwrap());
-        commit.set(nfs_stat.commit.try_into().unwrap());
-
+        rpcs.with_label_values(&["Access"]).set(nfs_stat.access.try_into().unwrap());
+        rpcs.with_label_values(&["Commit"]).set(nfs_stat.commit.try_into().unwrap());
+        rpcs.with_label_values(&["Create"]).set(nfs_stat.create.try_into().unwrap());
+        rpcs.with_label_values(&["FsInfo"]).set(nfs_stat.fsinfo.try_into().unwrap());
+        rpcs.with_label_values(&["FsStat"]).set(nfs_stat.fsstat.try_into().unwrap());
+        rpcs.with_label_values(&["GetAttr"]).set(nfs_stat.getattr.try_into().unwrap());
+        rpcs.with_label_values(&["Link"]).set(nfs_stat.link.try_into().unwrap());
+        rpcs.with_label_values(&["Lookup"]).set(nfs_stat.lookup.try_into().unwrap());
+        rpcs.with_label_values(&["MkDir"]).set(nfs_stat.mkdir.try_into().unwrap());
+        rpcs.with_label_values(&["MkNod"]).set(nfs_stat.mknod.try_into().unwrap());
+        rpcs.with_label_values(&["PathConf"]).set(nfs_stat.pathconf.try_into().unwrap());
+        rpcs.with_label_values(&["Read"]).set(nfs_stat.read.try_into().unwrap());
+        rpcs.with_label_values(&["SetAttr"]).set(nfs_stat.setattr.try_into().unwrap());
+        rpcs.with_label_values(&["ReadDir"]).set(nfs_stat.readdir.try_into().unwrap());
+        rpcs.with_label_values(&["ReadDirPlus"]).set(nfs_stat.readdirplus.try_into().unwrap());
+        rpcs.with_label_values(&["ReadLink"]).set(nfs_stat.readlink.try_into().unwrap());
+        rpcs.with_label_values(&["Remove"]).set(nfs_stat.remove.try_into().unwrap());
+        rpcs.with_label_values(&["Rename"]).set(nfs_stat.rename.try_into().unwrap());
+        rpcs.with_label_values(&["RmDir"]).set(nfs_stat.rmdir.try_into().unwrap());
+        rpcs.with_label_values(&["SymLink"]).set(nfs_stat.symlink.try_into().unwrap());
+        rpcs.with_label_values(&["Write"]).set(nfs_stat.write.try_into().unwrap());
         // Notify exporter that all metrics have been updated so the caller client can
         // receive a response.
         finished_sender.send(FinishedUpdate).unwrap();
