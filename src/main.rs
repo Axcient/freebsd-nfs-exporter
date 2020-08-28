81f2690a098bda5aa5a3ad8ae829ab46b8120626
Alan Somers <asomers@axcient.com>
2020-08-28 23:25:25 +0000

    Add command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // vim: tw=80
 
+use clap::{self, App, Arg};
 use env_logger::{
     Builder,
     Env,
@@ -17,12 +18,46 @@ use std::{
 mod nfs;
 
 fn main() {
+    let matches = App::new("nfs-exporter")
+        .version("0.1")
+        .about("Export NFS statistics to Prometheus")
+        .arg(Arg::with_name("bind")
+             .short("b")
+             .value_name("ADDR")
+             .help("Bind to this local address")
+             .takes_value(true))
+        .arg(Arg::with_name("client")
+             .short("c")
+             .help("Publish NFS client statistics"))
+        .arg(Arg::with_name("server")
+             .short("s")
+             .help("Publish NFS server statistics"))
+        .arg(Arg::with_name("port")
+             .short("p")
+             .value_name("PORT")
+             .help("TCP port (default 9898)")
+             .takes_value(true))
+        .get_matches();
+    let addr = matches.value_of("bind").unwrap_or("0.0.0.0");
+    let port = matches.value_of("port").unwrap_or("9898");
+    let (_c, s) = if !matches.is_present("client") && !matches.is_present("server") {
+        // By default, print everything
+        (true, true)
+    } else if matches.is_present("server") {
+        (false, true)
+    } else {
+        clap::Error::with_description(
+            "client stats are TODO",
+            clap::ErrorKind::InvalidValue)
+            .exit();
+    };
+
     // Setup logger with default level info so we can see the messages from
     // prometheus_exporter.
     Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // Parse address used to bind exporter to.
-    let addr_raw = "0.0.0.0:9185";
+    let addr_raw = format!("{}:{}", addr, port);
     let addr: SocketAddr = addr_raw.parse().expect("can not parse listen addr");
 
     // Start exporter.
@@ -51,86 +86,88 @@ fn main() {
         // Update metric with random value.
         let nfs_stat = nfs::collect().unwrap();
 
-        macro_rules! set_rpcs {
-            ($label:ident, $field:ident) => {
-                rpcs.with_label_values(&[stringify!($label)])
-                    .set(nfs_stat.$field.try_into().unwrap());
-            };
-        }
+        if s {
+            macro_rules! set_rpcs {
+                ($label:ident, $field:ident) => {
+                    rpcs.with_label_values(&[stringify!($label)])
+                        .set(nfs_stat.$field.try_into().unwrap());
+                };
+            }
 
-        bytes.with_label_values(&["read"])
-            .set(nfs_stat.bytes.read.try_into().unwrap());
-        bytes.with_label_values(&["write"])
-            .set(nfs_stat.bytes.write.try_into().unwrap());
-        duration.with_label_values(&["read"])
-            .set(nfs_stat.duration.read.try_into().unwrap());
-        duration.with_label_values(&["write"])
-            .set(nfs_stat.duration.write.try_into().unwrap());
-        duration.with_label_values(&["commit"])
-            .set(nfs_stat.duration.commit.try_into().unwrap());
-        set_rpcs!(Access, access);
-        set_rpcs!(BackChannelCtl, backchannelctrl);
-        set_rpcs!(BindConnToSess, bindconntosess);
-        set_rpcs!(Close, close);
-        set_rpcs!(Commit, commit);
-        set_rpcs!(Create, v3create);
-        set_rpcs!(CreateSession, createsess);
-        set_rpcs!(CreateV4, create);
-        set_rpcs!(DelegPurge, delegpurge);
-        set_rpcs!(DelegReturn, delegreturn);
-        set_rpcs!(DestroyClientId, destroyclid);
-        set_rpcs!(DestroySession, destroysess);
-        set_rpcs!(ExchangeId, exchangeid);
-        set_rpcs!(FreeStateId, freestateid);
-        set_rpcs!(FsInfo, fsinfo);
-        set_rpcs!(FsStat, fsstat);
-        set_rpcs!(GetAttr, getattr);
-        set_rpcs!(GetDevInfo, getdevinfo);
-        set_rpcs!(GetDevList, getdevlist);
-        set_rpcs!(GetDirDeleg, getdirdeleg);
-        set_rpcs!(GetFH, getfh);
-        set_rpcs!(LayoutCommit, layoutcommit);
-        set_rpcs!(LayoutGet, layoutget);
-        set_rpcs!(LayoutReturn, layoutreturn);
-        set_rpcs!(Link, link);
-        set_rpcs!(Lock, lock);
-        set_rpcs!(LockT, lockt);
-        set_rpcs!(LockU, locku);
-        set_rpcs!(Lookup, lookup);
-        set_rpcs!(LookupP, lookupp);
-        set_rpcs!(MkDir, mkdir);
-        set_rpcs!(MkNod, mknod);
-        set_rpcs!(Nverify, nverify);
-        set_rpcs!(Open, open);
-        set_rpcs!(OpenAttr, openattr);
-        set_rpcs!(OpenConfirm, openconfirm);
-        set_rpcs!(OpenDgrd, opendgrd);
-        set_rpcs!(PathConf, pathconf);
-        set_rpcs!(PutFH, putfh);
-        set_rpcs!(Read, read);
-        set_rpcs!(ReadDir, readdir);
-        set_rpcs!(ReadDirPlus, readdirplus);
-        set_rpcs!(ReadLink, readlink);
-        set_rpcs!(ReclaimCompl, reclaimcompl);
-        set_rpcs!(RelLockOwner, rellckown);
-        set_rpcs!(Remove, remove);
-        set_rpcs!(Rename, rename);
-        set_rpcs!(Renew, renew);
-        set_rpcs!(RestoreFH, restorefh);
-        set_rpcs!(RmDir, rmdir);
-        set_rpcs!(SaveFH, savefh);
-        set_rpcs!(SecInfo, secinfo);
-        set_rpcs!(SecInfoNoName, secinfononame);
-        set_rpcs!(Sequence, sequence);
-        set_rpcs!(SetAttr, setattr);
-        set_rpcs!(SetClientId, setclid);
-        set_rpcs!(SetClientIdConfirm, setclidcf);
-        set_rpcs!(SetSSV, setssv);
-        set_rpcs!(SymLink, symlink);
-        set_rpcs!(TestStateId, teststateid);
-        set_rpcs!(Verify, verify);
-        set_rpcs!(WantDeleg, wantdeleg);
-        set_rpcs!(Write, write);
+            bytes.with_label_values(&["read"])
+                .set(nfs_stat.bytes.read.try_into().unwrap());
+            bytes.with_label_values(&["write"])
+                .set(nfs_stat.bytes.write.try_into().unwrap());
+            duration.with_label_values(&["read"])
+                .set(nfs_stat.duration.read.try_into().unwrap());
+            duration.with_label_values(&["write"])
+                .set(nfs_stat.duration.write.try_into().unwrap());
+            duration.with_label_values(&["commit"])
+                .set(nfs_stat.duration.commit.try_into().unwrap());
+            set_rpcs!(Access, access);
+            set_rpcs!(BackChannelCtl, backchannelctrl);
+            set_rpcs!(BindConnToSess, bindconntosess);
+            set_rpcs!(Close, close);
+            set_rpcs!(Commit, commit);
+            set_rpcs!(Create, v3create);
+            set_rpcs!(CreateSession, createsess);
+            set_rpcs!(CreateV4, create);
+            set_rpcs!(DelegPurge, delegpurge);
+            set_rpcs!(DelegReturn, delegreturn);
+            set_rpcs!(DestroyClientId, destroyclid);
+            set_rpcs!(DestroySession, destroysess);
+            set_rpcs!(ExchangeId, exchangeid);
+            set_rpcs!(FreeStateId, freestateid);
+            set_rpcs!(FsInfo, fsinfo);
+            set_rpcs!(FsStat, fsstat);
+            set_rpcs!(GetAttr, getattr);
+            set_rpcs!(GetDevInfo, getdevinfo);
+            set_rpcs!(GetDevList, getdevlist);
+            set_rpcs!(GetDirDeleg, getdirdeleg);
+            set_rpcs!(GetFH, getfh);
+            set_rpcs!(LayoutCommit, layoutcommit);
+            set_rpcs!(LayoutGet, layoutget);
+            set_rpcs!(LayoutReturn, layoutreturn);
+            set_rpcs!(Link, link);
+            set_rpcs!(Lock, lock);
+            set_rpcs!(LockT, lockt);
+            set_rpcs!(LockU, locku);
+            set_rpcs!(Lookup, lookup);
+            set_rpcs!(LookupP, lookupp);
+            set_rpcs!(MkDir, mkdir);
+            set_rpcs!(MkNod, mknod);
+            set_rpcs!(Nverify, nverify);
+            set_rpcs!(Open, open);
+            set_rpcs!(OpenAttr, openattr);
+            set_rpcs!(OpenConfirm, openconfirm);
+            set_rpcs!(OpenDgrd, opendgrd);
+            set_rpcs!(PathConf, pathconf);
+            set_rpcs!(PutFH, putfh);
+            set_rpcs!(Read, read);
+            set_rpcs!(ReadDir, readdir);
+            set_rpcs!(ReadDirPlus, readdirplus);
+            set_rpcs!(ReadLink, readlink);
+            set_rpcs!(ReclaimCompl, reclaimcompl);
+            set_rpcs!(RelLockOwner, rellckown);
+            set_rpcs!(Remove, remove);
+            set_rpcs!(Rename, rename);
+            set_rpcs!(Renew, renew);
+            set_rpcs!(RestoreFH, restorefh);
+            set_rpcs!(RmDir, rmdir);
+            set_rpcs!(SaveFH, savefh);
+            set_rpcs!(SecInfo, secinfo);
+            set_rpcs!(SecInfoNoName, secinfononame);
+            set_rpcs!(Sequence, sequence);
+            set_rpcs!(SetAttr, setattr);
+            set_rpcs!(SetClientId, setclid);
+            set_rpcs!(SetClientIdConfirm, setclidcf);
+            set_rpcs!(SetSSV, setssv);
+            set_rpcs!(SymLink, symlink);
+            set_rpcs!(TestStateId, teststateid);
+            set_rpcs!(Verify, verify);
+            set_rpcs!(WantDeleg, wantdeleg);
+            set_rpcs!(Write, write);
+        }
 
         // Notify exporter that all metrics have been updated so the caller client can
         // receive a response.
